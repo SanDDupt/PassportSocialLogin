d4ff5848c33363399bfd28b3b1d8be204ef4b5ee
Sandra Dupont <ssduptcom@gmail.com>
2019-01-09 20:16:34 +0100

    Login with Slack

---
+++ app.js
@@ -0,0 +1,154 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+
+const SlackStrategy = require("passport-slack").Strategy;
+
+// Require user model
+const User = require("./models/user");
+
+// Connection base de données
+mongoose
+  .connect(
+    "mongodb://localhost/passportsignuplogin",
+    { useNewUrlParser: true }
+  )
+  .then(x => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
+
+const app = express();
+
+// MIDDLEWARE SETUP
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "our-passport-local-strategy-app",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Serialize et deserialize
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Messages d'erreur flash
+app.use(flash());
+passport.use(
+  new LocalStrategy(
+    {
+      passReqToCallback: true
+    },
+    (req, username, password, next) => {
+      User.findOne({ username }, (err, user) => {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          return next(null, false, { message: "Incorrect username" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return next(null, false, { message: "Incorrect password" });
+        }
+
+        return next(null, user);
+      });
+    }
+  )
+);
+
+// Slack
+passport.use(
+  new SlackStrategy(
+    {
+      clientID: "2432150752.520234749733",
+      clientSecret: "d6c66ad9ecc51f54894c203f96d1ecf9"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ slackID: profile.id })
+        .then(user => {
+          if (err) {
+            return done(err);
+          }
+          if (user) {
+            return done(null, user);
+          }
+          const newUser = new User({
+            slackID: profile.id
+          });
+          newUser.save().then(user => {
+            done(null, newUser);
+          });
+        })
+        .catch(error => {
+          next(error);
+        });
+    }
+  )
+);
+
+// Express View engine setup
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// default value for title local
+app.locals.title = "Express - Generated with IronGenerator";
+
+// Routes
+const authRoutes = require("./routes/auth-routes");
+app.use("/", authRoutes);
+
+const index = require("./routes/index");
+app.use("/", index);
+
+module.exports = app;
---
+++ routes/auth-routes.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const authRoutes = express.Router();
+
+// User model
+const User = require("../models/user");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+// Add passport
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const ensureLogin = require("connect-ensure-login");
+
+// ROUTE SIGNUP : GET + POST
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", { message: "The username already exists" });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save(err => {
+        if (err) {
+          res.render("auth/signup", { message: "Something went wrong" });
+        } else {
+          res.redirect("/");
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+// ROUTE LOGIN : GET + POST
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
+
+authRoutes.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
+
+// 2 ROUTES SLACK : GET + GET
+authRoutes.get("/auth/slack", passport.authenticate("slack"));
+authRoutes.get("/auth/slack/callback", passport.authenticate("slack", {
+  successRedirect: "/private-page",
+  failureRedirect: "/"
+}));
+
+
+// ROUTE PRIVATE PAGE
+authRoutes.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("private", { user: req.user });
+});
+
+//ROUTE LOGOUT
+authRoutes.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+
+module.exports = authRoutes;
